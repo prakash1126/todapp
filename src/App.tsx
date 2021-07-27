c41206e5f8ce2fa95e7b91a553530e11de10fa41
prakash1126 <prahlad1126@gmail.com>
2021-07-27 19:41:07 +0545

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react';
+import './App.css';
+interface myItem{
+  mytodo:string;
+}
 function App() {
+  const [record, setRecord]=useState({mytodo:""}as  myItem);
+  const[item,setItem]=useState([{mytodo:""}] as myItem[]);
+  const  handleChange = (event:any)=>{
+    setRecord({...record,[event.target.name]:event.target.value});
+  }
+  const addItem =(event:any)=>{
+    var newItem=item;
+    newItem.push();
+    setItem([...item, record])
+  }
+  const deleteItem=(index: any)=>{
+    var newItem=item
+    newItem.splice(index,1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div><h1>TODO-LIST</h1></div>
+    <div> <input onChange={handleChange} type="text" name="mytodo" 
+          placeholder="Enter the item" value={record.mytodo}/>
+    <button onClick={addItem}>Add</button>
+    </div> 
+    <div>
+      <h3>Todo list items</h3>
+      <ol>{item.map((items,index)=><li key={index}>
+        <span>{items.mytodo}</span> 
+        <button>Update</button> 
+        <button onClick={deleteItem}>Delete</button></li>)}
+      </ol>
     </div>
+    </>
   );
 }
-
 export default App;
